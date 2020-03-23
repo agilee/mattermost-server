7e48d3b247ff5ce44356739a98c98e62eb9b3722
Martin Kraft <martin@upspin.org>
2020-03-23 18:06:18 -0400

    WIP

--- evans/evans.go
+++ evans/evans.go
@@ -22,22 +22,3 @@ func (c Chain) Then(h web.ContextHandlerFunc) web.ContextHandlerFunc {
 
 	return h
 }
-
-func (c Chain) ThenFunc(fn web.ContextHandlerFunc) web.ContextHandlerFunc {
-	if fn == nil {
-		return c.Then(nil)
-	}
-	return c.Then(fn)
-}
-
-func (c Chain) Append(constructors ...Constructor) Chain {
-	newCons := make([]Constructor, 0, len(c.constructors)+len(constructors))
-	newCons = append(newCons, c.constructors...)
-	newCons = append(newCons, constructors...)
-
-	return Chain{newCons}
-}
-
-func (c Chain) Extend(chain Chain) Chain {
-	return c.Append(chain.constructors...)
-}
